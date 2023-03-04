e50300448b7993ba0b973bdabf79e95566b75f0f
jsaba101 <104746010+jsaba101@users.noreply.github.com>
2023-03-04 19:32:42 +1030

    Add files via upload

---
+++ Player.cpp
@@ -0,0 +1,12 @@
+#include "Player.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+char Player::makeMove(){
+    return move;
+};
+
+string Player::getName(){
+    return name;
+};
