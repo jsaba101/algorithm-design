42b030eafc3dd854b7b0ad14e2d97462e137cb7a
jsaba101 <104746010+jsaba101@users.noreply.github.com>
2023-03-04 21:14:19 +1030

    Add files via upload

--- Referee.cpp
+++ Referee.cpp
@@ -9,25 +9,25 @@ Player * Referee::refGame(Player * player1, Player * player2){
     char player1move = player1->makeMove();
     char player2move = player2->makeMove();
     if (player1move == 'R' && player2move == 'S'){
-        cout << player1->name << " ";
+        return player1;
     }
     else if (player1move == 'P' && player2move == 'R'){
-        cout << player1->name << " ";
+        return player1;
     }
     else if (player1move == 'S' && player2move == 'P'){
-        cout << player1->name << " ";
+        return player1;
     }
     else if (player2move == 'R' && player1move == 'S'){
-        cout << player2->name << " ";
+        return player2;
     }
     else if (player2move == 'P' && player1move == 'R'){
-        cout << player2->name << " ";
+        return player2;
     }
     else if (player2move == 'S' && player1move == 'P'){
-        cout << player2->name << " ";
+        return player2;
     }
     else if (player1move == player2move){
-        cout << "Tie ";
+        cout << "Tie " << endl;
         return nullptr;
     }
 }
