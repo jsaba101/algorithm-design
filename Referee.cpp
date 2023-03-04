9f39c82e2190e81565459759927726f651d6e75d
jsaba101 <104746010+jsaba101@users.noreply.github.com>
2023-03-04 20:51:52 +1030

    Add files via upload

--- Referee.cpp
+++ Referee.cpp
@@ -6,25 +6,27 @@ using namespace std;
 Referee::Referee(){} 
 
 Player * Referee::refGame(Player * player1, Player * player2){
-    if (player1->move == 'R' && player2->move == 'S'){
+    char player1move = player1->makeMove();
+    char player2move = player2->makeMove();
+    if (player1move == 'R' && player2move == 'S'){
         cout << player1->name << " wins!";
     }
-    else if (player1->move == 'P' && player2->move == 'R'){
+    else if (player1move == 'P' && player2move == 'R'){
         cout << player1->name << " wins!";
     }
-    else if (player1->move == 'S' && player2->move == 'P'){
+    else if (player1move == 'S' && player2move == 'P'){
         cout << player1->name << " wins!";
     }
-    else if (player2->move == 'R' && player1->move == 'S'){
+    else if (player2move == 'R' && player1move == 'S'){
         cout << player2->name << " wins!";
     }
-    else if (player2->move == 'P' && player1->move == 'R'){
+    else if (player2move == 'P' && player1move == 'R'){
         cout << player2->name << " wins!";
     }
-    else if (player2->move == 'S' && player1->move == 'P'){
+    else if (player2move == 'S' && player1move == 'P'){
         cout << player2->name << " wins!";
     }
-    else if (player1->move == player2->move){
+    else if (player1move == player2move){
         return nullptr;
     }
     return NULL;
