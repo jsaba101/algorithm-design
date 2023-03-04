23caabd1442a51e6087e0bd53a8280bca56c97c4
jsaba101 <104746010+jsaba101@users.noreply.github.com>
2023-03-04 20:56:11 +1030

    Add files via upload

--- Human.cpp
+++ Human.cpp
@@ -14,6 +14,7 @@ Human::Human(string hname){
 char Human::makeMove(){
     cout << "Enter Move: ";
     cin >> move;
+    cout << endl;
     return move;
 }
 
--- Referee.cpp
+++ Referee.cpp
@@ -9,24 +9,25 @@ Player * Referee::refGame(Player * player1, Player * player2){
     char player1move = player1->makeMove();
     char player2move = player2->makeMove();
     if (player1move == 'R' && player2move == 'S'){
-        cout << player1->name << " wins!";
+        cout << player1->name << " ";
     }
     else if (player1move == 'P' && player2move == 'R'){
-        cout << player1->name << " wins!";
+        cout << player1->name << " ";
     }
     else if (player1move == 'S' && player2move == 'P'){
-        cout << player1->name << " wins!";
+        cout << player1->name << " ";
     }
     else if (player2move == 'R' && player1move == 'S'){
-        cout << player2->name << " wins!";
+        cout << player2->name << " ";
     }
     else if (player2move == 'P' && player1move == 'R'){
-        cout << player2->name << " wins!";
+        cout << player2->name << " ";
     }
     else if (player2move == 'S' && player1move == 'P'){
-        cout << player2->name << " wins!";
+        cout << player2->name << " ";
     }
     else if (player1move == player2move){
+        cout << "Tie ";
         return nullptr;
     }
     return NULL;
