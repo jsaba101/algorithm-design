1dd77e1e0eb5a1259e49dafaa60e8a54349da20b
jsaba101 <104746010+jsaba101@users.noreply.github.com>
2023-03-20 21:56:43 +1030

    Update Truckloads.cpp

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -1,10 +1,11 @@
 #include "Truckloads.h"
 
 int Truckloads::numTrucks(int numCrates, int loadSize){
-    //checks if the number of crates is less than the load size and returns 1;
+    //checks if the number of crates is less than the load size and returns 1 if true;
     if (numCrates <= loadSize){
         return 1;
     }
-    //recursively returns numtrucks
+    //recursively returns numTrucks by calling function on numCrates divided by 2 + remainder of numCrates divided by 2
+    //with loadSize staying the same, and adding another call of numTrucks with numCrates divided by 2
     return numTrucks(numCrates / 2 + numCrates % 2, loadSize) + numTrucks(numCrates / 2, loadSize);
 }
