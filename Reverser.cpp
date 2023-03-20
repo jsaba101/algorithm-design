a7f21c8b6be63bd28e940dd60c809c3e10ff9482
jsaba101 <104746010+jsaba101@users.noreply.github.com>
2023-03-20 14:12:14 +1030

    Add files via upload

--- Reverser.cpp
+++ Reverser.cpp
@@ -1,21 +1,22 @@
 #include "Reverser.h"
 #include <string>
-#include <iostream>
 #include <math.h>
 
 using namespace std;
 
 int Reverser::reverseDigit(int n){
     int integer = log10(n);
-    //checks if number is less than 1 and returns 0;
-    if (n == 0){
-        return 0;
-    }
-    else if (n < 0){
+    //checks if number is less than 0 and returns -1;
+    if (n < 0){
         return -1;
     }
-    //otherwise recursively calls function
-    return ((n % 10 * pow(10, integer)) + reverseDigit(n / 10));
+    //checks if n is less than 10 and returns input
+    else if (n < 10){
+        return n;
+    }
+    //otherwise reverses digit by dividing input by 10 and adding the remainder of the input divided by 10
+    //times the power of 10 to the integer value
+    return (reverseDigit(n / 10) + (n % 10 * pow(10, integer)));
 }
 
 string Reverser::reverseString(string input){
