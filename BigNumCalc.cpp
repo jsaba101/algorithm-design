f5bf0c140117e64d8e7fbb9b19ebdd5d63541324
jsaba101 <104746010+jsaba101@users.noreply.github.com>
2023-04-29 19:48:11 +0930

    Add files via upload

---
+++ BigNumCalc.cpp
@@ -0,0 +1,30 @@
+#include "BigNumCalc.h"
+#include <iostream>
+
+using namespace std;
+
+BigNumCalc::BigNumCalc(){
+    head = nullptr;
+}
+
+BigNumCalc::~BigNumCalc(){}
+
+list<int> BigNumCalc::buildBigNum(string numString){
+    if (numString.size() == 0){
+        return;
+    }
+
+    Node *currNode = head;
+}
+
+list<int> BigNumCalc::add(list<int> num1, list<int> num2){
+
+}
+
+list<int> BigNumCalc::sub(list<int> num1, list<int> num2){
+
+}
+
+list<int> BigNumCalc::mul(list<int> num1, list<int> num2){
+    
+}
